76acdd093f0b3a793b517e464b2a96550d5ef2e5
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2020-10-08 12:19:52 +0800

    debug

--- newifi3.sh
+++ newifi3.sh
@@ -7,7 +7,13 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-sed -i 's/#src-git helloworld https://github.com/fw876/helloworld/src-git helloworld https://github.com/fw876/helloworld/g' /feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld/src-git' feeds.conf.default
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 
 #移除不用软件包
 #rm -rf xxx/xxx/xxx/文件名
@@ -15,9 +21,6 @@ rm -rf lede/package/lean/luci-theme-argon
 
 #添加额外软件包
 git clone https://github.com/cnzd/luci-app-koolproxyR.git         #去广告最新KPR
-git clone https://github.com/xiaorouji/openwrt-package.git        #常用软件集合包
-git clone https://github.com/kenzok8/openwrt-packages.git         #常用软件集合包二
-git clone https://github.com/kenzok8/small.git
 git clone https://github.com/destan19/OpenAppFilter.git           #APP过滤应用
 git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
 
