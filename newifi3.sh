eb9f881d1bf55009fbbb095df7fe79b9dc810ea1
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2020-10-08 11:57:03 +0800

    适配新主题

--- newifi3.sh
+++ newifi3.sh
@@ -11,6 +11,7 @@ sed -i 's/#src-git helloworld https://github.com/fw876/helloworld/src-git hellow
 
 #移除不用软件包
 #rm -rf xxx/xxx/xxx/文件名
+rm -rf lede/package/lean/luci-theme-argon
 
 #添加额外软件包
 git clone https://github.com/cnzd/luci-app-koolproxyR.git         #去广告最新KPR
