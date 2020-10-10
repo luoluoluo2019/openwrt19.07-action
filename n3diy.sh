36fcc9ba7029e762726b71a8e71b6f8f820d05cd
luoluoluo2019 <62978078+luoluoluo2019@users.noreply.github.com>
2020-10-11 00:16:42 +0800

    Update n3diy.sh

--- n3diy.sh
+++ n3diy.sh
@@ -10,3 +10,4 @@ rm -rf lede/package/lean/luci-theme-argon
 git clone https://github.com/cnzd/luci-app-koolproxyR.git         #去广告最新KPR
 git clone https://github.com/destan19/OpenAppFilter.git           #APP过滤应用
 git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
+git clone https://github.com/vernesong/OpenClash.git
